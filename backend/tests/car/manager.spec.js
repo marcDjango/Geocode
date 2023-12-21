fe07150a01f2bdc7098e8adee7e1a418c2449cf9
Khachik090909 <khachiksahakyan90@gmail.com>
2023-12-21 10:49:40 +0100

    test: preparation

---
+++ backend/tests/car/manager.spec.js
@@ -0,0 +1,37 @@
+// Import required dependencies
+const { database, tables } = require("../setup");
+
+// Test suite for the create method of ItemManager
+describe("Create car", () => {
+  it("should create an car successfully", async () => {
+    // Define a sample car for testing
+    const testCar = {
+      title: "Sample car",
+    };
+
+    // Send a create request to the car table with a test car
+    const insertId = await tables.car.create(testCar);
+
+    // Check if the newly added car exists in the database
+    const [rows] = await database.query(
+      "select * from car where id = ?",
+      insertId
+    );
+
+    const foundCar = rows[0];
+
+    // Assertions
+    expect(foundCar).toBeDefined();
+    expect(foundCar.title).toBe(testCar.title);
+  });
+
+  it("should throw when passing invalid object", async () => {
+    // Thx https://jestjs.io/docs/asynchronous#asyncawait
+
+    // Send a create request to the item table with an empty object
+    const promise = tables.car.create({});
+
+    // Assertions
+    await expect(promise).rejects.toThrow();
+  });
+});
