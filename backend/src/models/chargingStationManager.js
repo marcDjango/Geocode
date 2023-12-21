4ed6b29dca1b437d80dd04c828a0df8d89ad6095
Marcelo <marceloromero2016ar@gmail.com>
2023-12-21 10:26:00 +0100

    chore: finish CRUD chargin station

--- backend/src/models/chargingStationManager.js
+++ backend/src/models/chargingStationManager.js
@@ -1,67 +1,104 @@
+// Importing the AbstractManager class
 const AbstractManager = require("./AbstractManager");
 
-class chargingStationManager extends AbstractManager {
+// Defining the chargingStationManager class that extends AbstractManager
+class ChargingStationManager extends AbstractManager {
+  // Constructor initializes the class and sets the table name to "charging_station"
   constructor() {
+    // Calling the constructor of the parent class (AbstractManager) with the table name
     super({ table: "charging_station" });
   }
 
+  // Method to read a charging station record by its ID
   async read(id) {
+    // Performing a database query to select a record with the given ID
     const [row] = await this.database.query(
-      `select * from ${this.table} where id =?`,
+      `select * from ${this.table} where id = ?`,
       [id]
     );
+    // Returning the first row (assuming there is only one result)
     return row[0];
   }
 
+  // Method to read all charging station records
   async readAll() {
+    // Performing a database query to select all records from the charging station table
     const [rows] = await this.database.query(`select * from ${this.table}`);
-
+    // Returning all rows
     return rows;
   }
 
+  // Method to edit/update a charging station record by ID
   async edit(body, id) {
+    // Function to check if a value is an array and stringify it if necessary
     function isArray(data) {
       return Object.values(data).map((arr) =>
         Array.isArray(arr) ? JSON.stringify(arr) : arr
       );
     }
 
+    // Converting values in the body to an array (stringifying arrays if present)
     const values = isArray(body);
+
+    // Getting the keys (parameters) from the body
     const params = Object.keys(body);
+
+    // Creating a string of parameters for the SQL SET clause
     const setParams = params.map((item) => `${item} = ?`).join(", ");
 
+    // Performing a database query to update the record with the given ID
     const [rows] = await this.database.query(
       `UPDATE ${this.table} SET ${setParams} WHERE id = ?`,
       [...values, id]
     );
+
+    // Returning the result of the update operation
     return rows;
   }
 
+  // Method to add/insert a new charging station record
   async add(body) {
+    // Function to check if a value is an array and stringify it if necessary
     function isArray(data) {
       return Object.values(data).map((arr) =>
         Array.isArray(arr) ? JSON.stringify(arr) : arr
       );
     }
 
+    // Converting values in the body to an array (stringifying arrays if present)
     const values = isArray(body);
+
+    // Getting the keys (parameters) from the body
     const params = Object.keys(body);
-    const setParams = params.map((item) => item).join(", ");
+
+    // Creating a string of parameters for the SQL INSERT INTO clause
+    const setParams = params.join(", ");
+
+    // Creating a placeholder string for the values in the SQL query
     const placeholder = params.map(() => "? ").join(", ");
 
+    // Performing a database query to insert a new record into the charging station table
     const [rows] = await this.database.query(
       `INSERT INTO ${this.table}(${setParams}) VALUES (${placeholder})`,
       [...values]
     );
+
+    // Returning the result of the insert operation
     return rows;
   }
 
+  // Method to delete a charging station record by its ID
   async delete(id) {
+    // Performing a database query to delete a record with the given ID
     const [rows] = await this.database.query(
       `delete from ${this.table} where id = ?`,
       [id]
     );
+
+    // Returning the result of the delete operation
     return rows;
   }
 }
-module.exports = chargingStationManager;
+
+// Exporting the ChargingStationManager class
+module.exports = ChargingStationManager;
