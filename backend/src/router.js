2d6df8e27793838b2ea1e3cb730e49f156449b17
Khachik090909 <khachiksahakyan90@gmail.com>
2023-12-25 17:10:55 +0100

    test:preparation pull

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,30 +8,17 @@ const router = express.Router();
 
 // Import itemControllers module for handling item-related operations
 
-const carControllers = require("./controllers/carControllers");
-
-// Route to get a list of items
-
-router.get("/cars", carControllers.browse);
-
-/* ************************************************************************* */
-// Import chargingStationControllers module for handling operations
 const chargingStationControllers = require("./controllers/chargingStationControllers");
+const carControllers = require("./controllers/carControllers");
 
 // Route to get a list of charging station
 router.get("/charging-station", chargingStationControllers.browse);
-
-// Route to get a specific item by ID
 router.get("/charging-station/:id", chargingStationControllers.read);
-
 router.put("/charging-station/:id", chargingStationControllers.edit);
-
-// Route to add a new item
 router.post("/charging-station/", chargingStationControllers.add);
-
 router.delete("/charging-station/:id", chargingStationControllers.destroy);
-/* ************************************************************************* */
 
+// Route to get a list of
 router.get("/cars", carControllers.browse);
 router.get("/cars/:id", carControllers.read);
 router.put("/cars/:id", carControllers.edit);
