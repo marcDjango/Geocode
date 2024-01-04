9261cdc6f6614b1e9971d2c12dde88f600be4625
Khachik090909 <khachiksahakyan90@gmail.com>
2024-01-04 16:03:06 +0100

    fix:routes.spec

--- backend/tests/car/manager.spec.js
+++ backend/tests/car/manager.spec.js
@@ -4,43 +4,43 @@ const { carCreate, carUpdate } = require("../testdata");
 
 describe("Create Car", () => {
   it("should create an Car successfully", async () => {
-    const resalt = await tables.car.add(carCreate);
+    const result = await tables.car.add(carCreate);
     const [rows] = await database.query(
       `select * from ${tables.car.table} where id = ?`,
-      [resalt]
+      [result]
     );
     expect(rows.length).toBe(1);
     return rows;
   });
 
   it("should read a charging Car successfully", async () => {
-    const resalt = await tables.car.add(carCreate);
+    const result = await tables.car.add(carCreate);
     const [Car] = await database.query(
       `select * from ${tables.car.table} where id = ?`,
-      [resalt]
+      [result]
     );
 
-    expect(Car[0].id).toEqual(resalt);
+    expect(Car[0].id).toEqual(result);
   });
 
   it("should update a charging Car successfully", async () => {
-    const resalt = await tables.car.add(carCreate);
-    await tables.car.edit(carUpdate, resalt);
+    const result = await tables.car.add(carCreate);
+    await tables.car.edit(carUpdate, result);
 
     const [updatedCar] = await database.query(
       `select * from ${tables.car.table} where id = ?`,
-      [resalt]
+      [result]
     );
 
     expect(updatedCar.someProperty).toEqual(carUpdate.someProperty);
   });
 
   it("should delete a charging Car successfully", async () => {
-    const resalt = await tables.car.add(carCreate);
-    await tables.car.delete(resalt);
+    const result = await tables.car.add(carCreate);
+    await tables.car.delete(result);
     const [deletedCar] = await database.query(
       `select * from ${tables.car.table} where id = ?`,
-      [resalt]
+      [result]
     );
 
     expect(deletedCar).toEqual([]);
