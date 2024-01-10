5bdfaf2c633520d42b8e5120d95076e9c668ad44
Marcelo <marceloromero2016ar@gmail.com>
2024-01-10 15:51:26 +0100

    feat: create form contact

---
+++ backend/src/controllers/contactControllers.js
@@ -0,0 +1,71 @@
+const tables = require("../tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const messages = await tables.contact.readAll();
+
+    res.status(200).json(messages);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const read = async (req, res, next) => {
+  try {
+    const message = await tables.contact.read(req.params.id);
+    if (!message) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(message);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const edit = async (req, res, next) => {
+  try {
+    const message = await tables.contact.edit(req.body, req.params.id);
+    if (message == null) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const message = await tables.contact.add(req.body);
+    if (!message) {
+      res.sendStatus(404);
+    } else {
+      res.status(201).json(message);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+    const result = await tables.contact.delete(req.params.id);
+    if (result.affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
